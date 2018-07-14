ce2d43e9dbb96d4720760877bb193b86a39c782f
sqmax <sqpolar@gmail.com>
2018-07-14 08:11:32 +0800

    m

--- src/js/main.js
+++ src/js/main.js
@@ -26,11 +26,10 @@ Hi! 我是SqMax
 目前是刚入行的野生程序猿
 
 - {C\\ , Java\\ }Programmer
-- Linux用户
-- 专注于Web后端
+- 专注于Java Web后端
 - 会一点点前端
 
-最近在搭建个人博客，一不小心就发现很多好玩的东西，感觉前端的水好深，把这个写完我就要赶紧上岸了，不然就耽误正业了。
+最近在搭建个人博客，一不小心就发现很多好玩的东西。
 
 ## Contact Me
 
@@ -38,12 +37,10 @@ Hi! 我是SqMax
 
 - [GitHub](https://www.github.com/sqmax)
 - [个人网站](http://www.sqmax.top)
-- [segmentfault](https://segmentfault.com/u/sqmax)
-- [掘金](https://juejin.im/user/5a97f7b86fb9a028b86d717c)
 - [新浪微博](http://weibo.com/sqmax)
 - [网易云音乐](http://music.163.com/#/user/home?id=1375636217)
+- [知乎](https://www.zhihu.com/people/sqmax)
 - [给我发email](mailto: sqpolar@gmail.com)
-
 `
 
 // CodeMirror editor
